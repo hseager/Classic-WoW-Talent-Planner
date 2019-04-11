ae3e2fc2653e0a6e6b1275ec8acd62c8db57b61b
hseager <harvey.seager@googlemail.com>
2019-04-11 21:01:48 +0100

    Completed skill points

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,6 +3,29 @@ Vue.component('class-selection', {
 	template: `<li><button v-on:click="$emit('change-class')">{{klass.name}}</button></li>`
 });
 
+Vue.component('class-trees', {
+	props: ['classTree'],
+	template: 
+	`<div class="talent-trees">
+		<strong>Skills points: {{classTree.skillPoints}}</strong>
+		<talent-tree 
+			v-for="tree in classTree.specs"
+			v-bind:tree="tree"
+			v-bind:key="tree.id"
+			v-on:decreaseTotalSkillPoints="decreaseTotalSkillPoints(classTree)"
+			v-on:increaseTotalSkillPoints="increaseTotalSkillPoints(classTree)"
+		></talent-tree>
+	</div>`,
+	methods: {
+		decreaseTotalSkillPoints: function(classTree){
+			classTree.skillPoints--;
+		},
+		increaseTotalSkillPoints: function(classTree){
+			classTree.skillPoints++;
+		}
+	}
+});
+
 Vue.component('talent-tree', {
 	props: ['tree'],
 	template: 
@@ -11,66 +34,72 @@ Vue.component('talent-tree', {
 		<skills
 			v-for="skill in tree.skills"
 			v-bind:skill="skill"
-			v-bind:key="skill.id"
-			v-on:increase-skill-rank="onIncreaseSkillRank(skill)"
-			v-on:decrease-skill-rank="onDecreaseSkillRank(skill)">
+			v-bind:key="skill.id">
 		></skills>
 	</div>`,
-	methods: {
-		onIncreaseSkillRank: function(skill){
-			if(skill.currentRank < skill.maxRank)
-				skill.currentRank++
-				this.talentTrees[this.currentClass].skillPoints--;
-		},
-		onDecreaseSkillRank: function(skill){
-			if(skill.currentRank >= 1)
-				skill.currentRank--;
-				this.talentTrees[this.currentClass].skillPoints++;
-		}
-	}
 });
  
 Vue.component('skills', {
 	props: ['skill'],
 	template: 
 		`<div class="skill"
-			v-on:click="$emit('increase-skill-rank')"
-			v-on:click.right.prevent="$emit('decrease-skill-rank')">
+			v-on:click="onIncreaseSkillRank(skill)"
+			v-on:click.right.prevent="onDecreaseSkillRank(skill)">
 			{{skill.name}}: {{skill.currentRank}}
-		</div>`
+		</div>`,
+	methods: {
+		onIncreaseSkillRank: function(skill){
+			if(skill.currentRank < skill.maxRank){
+				skill.currentRank++
+				this.$parent.$emit('decreaseTotalSkillPoints');
+			}
+		},
+		onDecreaseSkillRank: function(skill){
+			if(skill.currentRank >= 1){
+				skill.currentRank--;
+				this.$parent.$emit('increaseTotalSkillPoints');
+			}
+		}
+	}
 });
 
 var app = new Vue({
 	el: '#app',
 	data: {
-		currentClass: 'Hunter',
+		currentClass: 'hunter',
 		classes: [
-			{ id: 0, name: 'Hunter' },
-			{ id: 1, name: 'Rogue' },
-			{ id: 2, name: 'Mage' },
+			{ id: 0, name: 'hunter' },
+			{ id: 1, name: 'rogue' },
+			{ id: 2, name: 'mage' },
 		],
 		talentTrees: {
-			Hunter: {
+			hunter: {
 				skillPoints: 51,
-				specs : [
+				specs: [
 					{ id: 0, name: 'Beast Mastery', skills: [
 						{ id: 0, name: 'Improved Aspect of the Hawk', maxRank: 5, currentRank: 0 },
 						{ id: 1, name: 'Endurance Training', maxRank: 5, currentRank: 0 }
 					] },
 					{ id: 1, name: 'Markmanship' },
 					{ id: 2, name: 'Survival' }
-				],
+				]
 			},
-			Rogue: [
-				{ id: 0, name: 'Assassination' },
-				{ id: 1, name: 'Combat' },
-				{ id: 2, name: 'Subtlety' }
-			],
-			Mage: [
-				{ id: 0, name: 'Arcane' },
-				{ id: 1, name: 'Fire' },
-				{ id: 2, name: 'Frost' }
-			],
+			rogue: {
+				skillPoints: 51,
+				specs: [
+					{ id: 0, name: 'Assassination' },
+					{ id: 1, name: 'Combat' },
+					{ id: 2, name: 'Subtlety' }
+				]
+			},
+			mage: {
+				skillPoints: 51,
+				specs: [
+					{ id: 0, name: 'Arcane' },
+					{ id: 1, name: 'Fire' },
+					{ id: 2, name: 'Frost' }
+				]
+			} 
 		},
 	},
 });
