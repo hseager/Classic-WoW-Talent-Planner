1512e81d901a40d4629bfaa85cfdf54d21152586
hseager <harvey.seager@googlemail.com>
2019-04-12 13:49:25 +0100

    Improved structure

---
+++ assets/data/talent-data.js
@@ -0,0 +1,122 @@
+let talentData = {
+	"currentClass": 0,
+	"classes": [
+		{ 
+			"id": 0, 
+			"name": "hunter", 
+			"skillPoints": 51, 
+			"talentTrees": [
+				{ 
+					"id": 0,
+					"name": "Beast Mastery",
+					"skills": [
+						{ 
+							"id": 0, 
+							"name": "Improved Aspect of the Hawk", 
+							"maxRank": 5, 
+							"currentRank": 0
+						},
+						{ 
+							"id": 1,
+							"name": "Endurance Training",
+							"maxRank": 5,
+							"currentRank": 0
+						}
+					]
+				},
+				{ 
+					"id": 1,
+					"name": "Markmanship",
+					"skills": [
+						{ 
+							"id": 0,
+							"name": "Improved Concussive Shot",
+							"maxRank": 5,
+							"currentRank": 0
+						},
+						{ 
+							"id": 1, 
+							"name": "Efficiency", 
+							"maxRank": 5, 
+							"currentRank": 0
+						}
+					]
+				},
+				{ 
+					"id": 2,
+					"name": "Survival"
+				}
+			]
+		},
+		{ 
+			"id": 1, 
+			"name": "rogue", 
+			"skillPoints": 51, 
+			"talentTrees": [
+				{ 
+					"id": 0,
+					"name": "Assassination",
+					"skills": [
+						{ 
+							"id": 0, 
+							"name": "Improved Eviscerate", 
+							"maxRank": 5, 
+							"currentRank": 0
+						},
+					]
+				},
+				{ 
+					"id": 1,
+					"name": "Combat",
+					"skills": [
+						{ 
+							"id": 0,
+							"name": "Improved Gouge",
+							"maxRank": 5,
+							"currentRank": 0
+						},
+					]
+				},
+				{ 
+					"id": 2,
+					"name": "Subtlety"
+				}
+			]
+		},
+		{ 
+			"id": 2, 
+			"name": "mage", 
+			"skillPoints": 51, 
+			"talentTrees": [
+				{ 
+					"id": 0,
+					"name": "Arcane",
+					"skills": [
+						{ 
+							"id": 0, 
+							"name": "Arcane Subtlety", 
+							"maxRank": 2, 
+							"currentRank": 0
+						},
+					]
+				},
+				{ 
+					"id": 1,
+					"name": "Fire",
+					"skills": [
+						{ 
+							"id": 0,
+							"name": "Improved Fireball",
+							"maxRank": 5,
+							"currentRank": 0
+						},
+					]
+				},
+				{ 
+					"id": 2,
+					"name": "Frost"
+				}
+			]
+		}
+	]
+};
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,27 +1,150 @@
-Vue.component('class-selection', {
-	props: ['klass'],
-	template: `<li><button v-on:click="$emit('change-class')">{{klass.name}}</button></li>`
+let talentData = {
+	"currentClass": 0,
+	"classes": [
+		{ 
+			"id": 0, 
+			"name": "hunter", 
+			"skillPoints": 51, 
+			"talentTrees": [
+				{ 
+					"id": 0,
+					"name": "Beast Mastery",
+					"skills": [
+						{ 
+							"id": 0, 
+							"name": "Improved Aspect of the Hawk", 
+							"maxRank": 5, 
+							"currentRank": 0
+						},
+						{ 
+							"id": 1,
+							"name": "Endurance Training",
+							"maxRank": 5,
+							"currentRank": 0
+						}
+					]
+				},
+				{ 
+					"id": 1,
+					"name": "Markmanship",
+					"skills": [
+						{ 
+							"id": 0,
+							"name": "Improved Concussive Shot",
+							"maxRank": 5,
+							"currentRank": 0
+						},
+						{ 
+							"id": 1, 
+							"name": "Efficiency", 
+							"maxRank": 5, 
+							"currentRank": 0
+						}
+					]
+				},
+				{ 
+					"id": 2,
+					"name": "Survival"
+				}
+			]
+		},
+		{ 
+			"id": 1, 
+			"name": "rogue", 
+			"skillPoints": 51, 
+			"talentTrees": [
+				{ 
+					"id": 0,
+					"name": "Assassination",
+					"skills": [
+						{ 
+							"id": 0, 
+							"name": "Improved Eviscerate", 
+							"maxRank": 5, 
+							"currentRank": 0
+						},
+					]
+				},
+				{ 
+					"id": 1,
+					"name": "Combat",
+					"skills": [
+						{ 
+							"id": 0,
+							"name": "Improved Gouge",
+							"maxRank": 5,
+							"currentRank": 0
+						},
+					]
+				},
+				{ 
+					"id": 2,
+					"name": "Subtlety"
+				}
+			]
+		},
+		{ 
+			"id": 2, 
+			"name": "mage", 
+			"skillPoints": 51, 
+			"talentTrees": [
+				{ 
+					"id": 0,
+					"name": "Arcane",
+					"skills": [
+						{ 
+							"id": 0, 
+							"name": "Arcane Subtlety", 
+							"maxRank": 2, 
+							"currentRank": 0
+						},
+					]
+				},
+				{ 
+					"id": 1,
+					"name": "Fire",
+					"skills": [
+						{ 
+							"id": 0,
+							"name": "Improved Fireball",
+							"maxRank": 5,
+							"currentRank": 0
+						},
+					]
+				},
+				{ 
+					"id": 2,
+					"name": "Frost"
+				}
+			]
+		}
+	]
+};
+
+Vue.component('class-list', {
+	props: ['classType'],
+	template: `<li><button v-on:click="$emit('change-class')">{{classType.name}}</button></li>`
 });
 
-Vue.component('class-trees', {
-	props: ['classTree'],
+Vue.component('class-panel', {
+	props: ['classType'],
 	template: 
 	`<div class="talent-trees">
-		<strong>Skills points: {{classTree.skillPoints}}</strong>
+		<strong>Skills points: {{classType.skillPoints}}</strong>
 		<talent-tree 
-			v-for="tree in classTree.specs"
+			v-for="tree in classType.talentTrees"
 			v-bind:tree="tree"
 			v-bind:key="tree.id"
-			v-on:decreaseTotalSkillPoints="decreaseTotalSkillPoints(classTree)"
-			v-on:increaseTotalSkillPoints="increaseTotalSkillPoints(classTree)"
+			v-on:decreaseClassSkillPoints="decreaseClassSkillPoints(classType)"
+			v-on:increaseClassSkillPoints="increaseClassSkillPoints(classType)"
 		></talent-tree>
 	</div>`,
 	methods: {
-		decreaseTotalSkillPoints: function(classTree){
-			classTree.skillPoints--;
+		decreaseClassSkillPoints: function(classType){
+			classType.skillPoints--;
 		},
-		increaseTotalSkillPoints: function(classTree){
-			classTree.skillPoints++;
+		increaseClassSkillPoints: function(classType){
+			classType.skillPoints++;
 		}
 	}
 });
@@ -51,55 +174,33 @@ Vue.component('skills', {
 		onIncreaseSkillRank: function(skill){
 			if(skill.currentRank < skill.maxRank){
 				skill.currentRank++
-				this.$parent.$emit('decreaseTotalSkillPoints');
+				this.$parent.$emit('decreaseClassSkillPoints');
 			}
 		},
 		onDecreaseSkillRank: function(skill){
 			if(skill.currentRank >= 1){
 				skill.currentRank--;
-				this.$parent.$emit('increaseTotalSkillPoints');
+				this.$parent.$emit('increaseClassSkillPoints');
 			}
 		}
 	}
 });
 
 var app = new Vue({
-	el: '#app',
-	data: {
-		currentClass: 'hunter',
-		classes: [
-			{ id: 0, name: 'hunter' },
-			{ id: 1, name: 'rogue' },
-			{ id: 2, name: 'mage' },
-		],
-		talentTrees: {
-			hunter: {
-				skillPoints: 51,
-				specs: [
-					{ id: 0, name: 'Beast Mastery', skills: [
-						{ id: 0, name: 'Improved Aspect of the Hawk', maxRank: 5, currentRank: 0 },
-						{ id: 1, name: 'Endurance Training', maxRank: 5, currentRank: 0 }
-					] },
-					{ id: 1, name: 'Markmanship' },
-					{ id: 2, name: 'Survival' }
-				]
-			},
-			rogue: {
-				skillPoints: 51,
-				specs: [
-					{ id: 0, name: 'Assassination' },
-					{ id: 1, name: 'Combat' },
-					{ id: 2, name: 'Subtlety' }
-				]
-			},
-			mage: {
-				skillPoints: 51,
-				specs: [
-					{ id: 0, name: 'Arcane' },
-					{ id: 1, name: 'Fire' },
-					{ id: 2, name: 'Frost' }
-				]
-			} 
-		},
-	},
+	el: '#talent-calculator',
+	template: 
+	`<div>
+		<ul>
+			<class-list
+				v-for="classType in classes"
+				v-bind:classType="classType"
+				v-bind:key="classType.id"
+				v-on:change-class="currentClass = classType.id"
+			></class-list>
+		</ul>
+		<class-panel
+			v-bind:class-type="classes[currentClass]"
+		></class-panel>
+	</div>`,
+	data: talentData,
 });
