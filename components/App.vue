f07ab3d95b7852f176cd95413cfe238a73778588
hseager <harvey.seager@googlemail.com>
2019-05-01 13:52:18 +0100

    Added local storage save

--- components/App.vue
+++ components/App.vue
@@ -4,25 +4,25 @@
 		<h1 class="main-title">Talent Calculator</h1>
 		<ul class="class-list">
 			<class-list
-				v-for="classType in classes"
+				v-for="classType in data.classes"
 				v-bind:classType="classType"
 				v-bind:key="classType.id"
-				v-on:change-class="currentClass = classType.id"
-				v-bind:constants="constants"
-				v-bind:currentClass="currentClass"
+				v-on:change-class="data.currentClass = classType.id"
+				v-bind:constants="data.constants"
+				v-bind:currentClass="data.currentClass"
 			></class-list>
 		</ul>
 		<class-panel
-			v-bind:class-type="classes[currentClass]"
-			v-bind:constants="constants"
+			v-bind:class-type="data.classes[data.currentClass]"
+			v-bind:constants="data.constants"
 		></class-panel>
 		<div class="talent-toolbar">
 			<div class="talent-info">
-				<p class="talent-info-stat">Skill points: {{classes[currentClass].availableSkillPoints}}</p>
-				<p class="talent-info-stat">Required level: {{classes[currentClass].requiredLevel}}</p>
+				<p class="talent-info-stat">Skill points: {{data.classes[data.currentClass].availableSkillPoints}}</p>
+				<p class="talent-info-stat">Required level: {{data.classes[data.currentClass].requiredLevel}}</p>
 			</div>
 			<div class="talent-actions">
-				<button class="button">Save</button>
+				<button class="button" v-on:click="saveTalentTrees()">Save</button>
 				<button class="button" v-on:click="resetTalentTrees()">Reset</button>
 			</div>
 		</div>
@@ -39,22 +39,35 @@
 			classPanel
 		},
 		data(){
-			return talentData;
+			return {
+				data: talentData
+			}
+		},
+		mounted(){
+			let localData = window.localStorage.getItem('talent-data');
+			if(localData != null){
+				this.data = JSON.parse(localData);
+			}
 		},
 		methods: {
 			resetTalentTrees: function(){
-				let currentClass = talentData.classes[this.currentClass];
+				let currentClass = this.data.classes[this.data.currentClass];
 				currentClass.availableSkillPoints = 51;
 				currentClass.requiredLevel = 0;
 				currentClass.talentTrees.forEach(tree => {
 					tree.skillPoints = 0;
 					tree.skills.forEach(skill => {
 						skill.currentRank = 0;
-						if(skill.requirements)
-						 	skill.enabled = false;
+						if(skill.requirements){
+							skill.enabled = false;
+						}
 					});
 				});
-			}
+				window.localStorage.removeItem('talent-data');
+			},
+			saveTalentTrees: function(){
+				window.localStorage.setItem('talent-data', JSON.stringify(this.data));
+			},
 		}
 	}
 </script>
